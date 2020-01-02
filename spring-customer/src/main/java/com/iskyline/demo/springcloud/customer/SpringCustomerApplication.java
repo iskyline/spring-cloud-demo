c6df03777b0e0a16f03c44815c2092fa78212437
i地平线 <iskyline23@163.com>
2020-01-02 19:41:50 +0800

    RestTemplate的ip形式调用

--- spring-customer/src/main/java/com/iskyline/demo/springcloud/customer/SpringCustomerApplication.java
+++ spring-customer/src/main/java/com/iskyline/demo/springcloud/customer/SpringCustomerApplication.java
@@ -3,6 +3,8 @@ package com.iskyline.demo.springcloud.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class SpringCustomerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCustomerApplication.class,args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
